779f0c57e19a60ed44f76a8145cf658e8c81a2e2
Taewon Shim <marocchino@Taewon-Shims-MacBook-Air.local>
2011-04-14 21:55:10 +0900

    처음 커밋 >_<

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chamber::Application.config.secret_token = '1ba4595a25cae2d09715974177b375bd4fd5b36f3c720879924c33eb491974c1b117fbcfdf9db8215a1ac3bd962bfab234abed48bfdfadf5f224f93362b9b5f5'
