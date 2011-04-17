b02726cf23a18f5b5cdd170dea78317c3d1362b5
Shim Taewon <marocchino@gmail.com>
2011-04-17 20:46:46 +0900

    책 골격 작성

---
+++ db/migrate/20110417110101_alter_version_to_book.rb
@@ -0,0 +1,10 @@
+class AlterVersionToBook < ActiveRecord::Migration
+  def self.up
+    change_column(:books, :version, :string, :limit => 4, :default => "0.1")
+    
+  end
+
+  def self.down
+    change_column(:books, :version, :integer)
+  end
+end
