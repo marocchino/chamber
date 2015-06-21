662b56e82126ae3ae5e1cae40b28f57ac12a3bee
Shim Won <marocchino@gmail.com>
2015-06-22 00:18:13 +0900

    Remove cucumber

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,41 +9,43 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_110_417_231_954) do
-  create_table 'books', force: true do |t|
-    t.string 'title'
-    t.string 'tag',        limit: 4, default: '0.1'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+ActiveRecord::Schema.define(version: 20110417231954) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "tag",        limit: 4, default: "0.1"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'chapters', force: true do |t|
-    t.string 'title'
-    t.integer 'book_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "chapters", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'versions', force: true do |t|
-    t.integer 'versioned_id'
-    t.string 'versioned_type'
-    t.integer 'user_id'
-    t.string 'user_type'
-    t.string 'user_name'
-    t.text 'modifications'
-    t.integer 'number'
-    t.integer 'reverted_from'
-    t.string 'tag'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "versions", force: :cascade do |t|
+    t.integer  "versioned_id"
+    t.string   "versioned_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "user_name"
+    t.text     "modifications"
+    t.integer  "number"
+    t.integer  "reverted_from"
+    t.string   "tag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'versions', ['created_at'], name: 'index_versions_on_created_at'
-  add_index 'versions', ['number'], name: 'index_versions_on_number'
-  add_index 'versions', ['tag'], name: 'index_versions_on_tag'
-  add_index 'versions', %w(user_id user_type), name: 'index_versions_on_user_id_and_user_type'
-  add_index 'versions', ['user_name'], name: 'index_versions_on_user_name'
-  add_index 'versions', %w(versioned_id versioned_type), name: 'index_versions_on_versioned_id_and_versioned_type'
+  add_index "versions", ["created_at"], name: "index_versions_on_created_at"
+  add_index "versions", ["number"], name: "index_versions_on_number"
+  add_index "versions", ["tag"], name: "index_versions_on_tag"
+  add_index "versions", ["user_id", "user_type"], name: "index_versions_on_user_id_and_user_type"
+  add_index "versions", ["user_name"], name: "index_versions_on_user_name"
+  add_index "versions", ["versioned_id", "versioned_type"], name: "index_versions_on_versioned_id_and_versioned_type"
+
 end
